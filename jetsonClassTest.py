6291a76b771dd814fbbc837a19dfaec58bbef5b1
Quang Vu <33469486+shirmplord@users.noreply.github.com>
2020-05-01 16:18:32 +0300

    Update jetsonClassTest.py
    move the original file to getDataFromRedis

--- jetsonClassTest.py
+++ jetsonClassTest.py
@@ -4,11 +4,17 @@
 ###
 
 class RPi:
-    __username = ""
-    __password = ""
-    def __init__(self):
+    __username = ""     #username for the php server
+    __password = ""     #password for the php server
+    __ID = ""           #ID of the pi (initiated value)
+    __Data_count = ""   #The number of the photo from a pi (incremented value)
+    __r = ""            #redis object
+    def __init__(self, ID):
         __username = "DBinnovation"
         __password = "DBinnovation123"
+        __r = gRedis
+        __ID = ID
+        __Data_count = 0
     def connectToServer(self, username, password):    
         self.__username = username
         self.__password = password
@@ -21,5 +27,16 @@ class RPi:
         mariadb_connection.commit()
         cursor.close()
         
-    def getDataFromRedis(self, )
-    def executeCommand(self, )
+    def getDataFromRedis(self, r)
+        img_bytes = r.get('imagedata_%d_%d',%self.__ID, %self.__Data_count)
+        if (img_bytes):
+            decoded = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), 1)
+            cv2.imwrite("retTest_%d_%d.jpg", %self.__ID, %self.__Data_count, decoded)
+            
+            fileName = ('retTest_%d_%d', %self.__ID, %self.__Data_count)
+            command = './alpr.sh {}'.format(quote(fileName))
+            print(command)
+            subprocess.call(shlex.split(command))
+            command = ""
+            self.__Data_count+=1
+    #def executeCommand(self)
